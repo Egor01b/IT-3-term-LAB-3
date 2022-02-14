7cde0817c6d75076b49b2d3ff16f4ccf7fcb7946
Myroslav Kryvoruchko <90711690+kmirik999@users.noreply.github.com>
2022-02-14 21:34:34 +0200

    change program.cs

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -1,12 +1,15 @@
 ﻿using Lab3_for_IT;
-using KeyValuePair = Lab3_for_IT.KeyValuePair;
 
-var list = new LinkedList();
-list.Add(new KeyValuePair("key1","value1"));
-list.Add(new KeyValuePair("key2","value2"));
-list.Add(new KeyValuePair("key3","value3"));
-Console.WriteLine(list.GetItemWithKey("key1").Value);
-list.RemoveByKey("key2");
-Console.WriteLine(list.GetItemWithKey("key3").Value);
-Console.WriteLine(list.GetItemWithKey("key2").Value);
-Console.WriteLine(list.GetItemWithKey("nothing").Value);
+
+var lines = File.ReadAllLines("dictionary.txt");
+var dictionary = new StringsDictionary();
+foreach (var line in lines)
+{
+   var index = line.IndexOf(';');
+   dictionary.Add(line.Substring(0, index), line.Substring(index + 1));
+   
+}
+
+Console.WriteLine(dictionary.Get("ABACUS"));
+Console.WriteLine(dictionary.Get("ABADDON"));
+File.ReadAllLines("dictionary.txt");
