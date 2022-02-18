c764930827062306293b32fc1c5beb2336425eb4
Egor Belous <belousegor5@gmail.com>
2022-02-18 20:49:17 +0200

    add Get Load and Expand Buckets

--- ConsoleApp6/StringsDictionary.cs
+++ ConsoleApp6/StringsDictionary.cs
@@ -8,6 +8,11 @@ public class StringsDictionary
 
     public void Add(string key, string value)
     {
+        if (GetLoad() > 0.75)
+        {
+            ExpandBuckets(_buckets.Length * 2);
+        }
+
         var bucket = CalculateHash(key) % _buckets.Length;
         if (_buckets[bucket] == null)
         {
@@ -40,6 +45,53 @@ public class StringsDictionary
         return null;
     }
 
+    private float GetLoad()
+    {
+        var full = 0;
+        for (int i = 0; i < _buckets.Length; i++)
+        {
+            if (_buckets[i] != null)
+            {
+                full++;
+            }
+        }
+
+        return full / _buckets.Length;
+    }
+
+    private void ExpandBuckets(int newCount)
+    {
+        var list = new LinkedList();
+        for (int i = 0; i < _buckets.Length; i++)
+        {
+            if (_buckets[i] != null)
+            {
+                // УУбать все элементы в List
+                while (true)
+                {
+                    var el = _buckets[i].PopFront();
+                    if (el == null) // если список пуст
+                    {
+                        break;
+                    }
+
+                    list.Add(el);
+                }
+            }
+        }
+
+        _buckets = new LinkedList[newCount];
+        while (true)
+        {
+            var el = list.PopFront();
+            if (el == null) // если список пуст
+            {
+                break;
+            }
+
+            Add(el.Key, el.Value);
+        }
+    }
 
     private int CalculateHash(string key)
     {
@@ -54,5 +106,4 @@ public class StringsDictionary
         result %= 2069;
         return result;
     }
-}
-
+}    
